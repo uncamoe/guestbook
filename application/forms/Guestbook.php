09a86bcac7930cd0bbaced0bf33d6c3792ea58e7
Stephen Nielsen <uncamoe@gmail.com>
2012-02-20 17:29:26 -0700

    Initial commit.

---
+++ application/forms/Guestbook.php
@@ -0,0 +1,52 @@
+<?php
+
+class Application_Form_Guestbook extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+        // set method for display form to post
+        $this->setMethod('post');
+        
+        // add email el
+        $this->addElement('text','email',array(
+            'label' => 'Your Email Address:',
+            'required' => true, 
+            'filters' => array('stringTrim'), 
+            'validators' => array(
+                'EmailAddress',
+                )
+            ));
+        // add comment element
+        $this->addElement('textarea','comment',  array(
+            'label' => 'Please Comment',
+            'required' => true,
+            'validators' => array(
+                array('validator' => 'StringLength', 'options' => array(0, 20))
+            )
+        ));
+        // add captcha
+        $this->addElement('captcha', 'captcha', array(
+            'label' => 'Please enter the following 5 letters below:',
+            'required' => true,
+            'captcha' => array(
+                'captcha' => 'Figlet',
+                'wordlen' => 5,
+                'timeout' => 300
+            )
+        ));
+        // add submit button
+        $this->addElement('submit', 'submit', array(
+            'ignore' => true,
+            'label' => 'Sign Guestbook',
+        ));
+        // add CSRF protection
+        $this->addElement('hash', 'csrf', array(
+            'ignore' => true,
+        ));
+    }
+
+
+}
+
---
+++ application/models/DBTable/Guestbook.php
@@ -0,0 +1,5 @@
+<?php
+    class Application_Model_DbTable_Guestbook extends Zend_Db_Table_Abstract {
+        protected $_name = 'guestbook';
+    }
+?>
